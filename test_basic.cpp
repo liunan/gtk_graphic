f4759a5089fb1e7ae3fb5a15cc16cd3cc3424c05
liunan <liunan@msn.com>
2011-07-03 23:36:47 +0800

    modified

--- test_basic.cpp
+++ test_basic.cpp
@@ -1,4 +1,4 @@
-#include <scene/basics.h>
+#include <core/point.h>
 #include <iostream>
 
 using namespace std;
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
   using namespace Graphic;
-  typedef Point<int> point_type;
+  typedef Point point_type;
   point_type point(100,100);
   cout<<point.GetX()<<","<<point.GetY()<<endl;
 	return 0;
