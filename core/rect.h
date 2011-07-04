521f9f71021dee9e81dfac4edde9c9e655e6750f
liunan <liunan@msn.com>
2011-07-04 22:02:20 +0800

    Signed-off-by: liunan <liunan@msn.com>

--- core/rect.h
+++ core/rect.h
@@ -1,7 +1,8 @@
 #ifndef LN_RECT__H
 #define LN_RECT__H
 #include "config.h"
-#include "Point.h"
+#include "point.h"
+#include <iostream>
 
 BEGIN_NAMESPACE
 class Rect{
@@ -14,8 +15,8 @@ public:
 	Point Center()const;
 	int GetWidth()const{return (m_right -m_left +1);}
 	int GetHeight()const{return (m_bottom -m_top +1);}
-	void SetLeftTop(const Point& p);
-	void SetRightBottom(const Point& p);
+	void SetLeftTop(const Point& p){m_left = p.GetX();m_top = p.GetY();}
+	void SetRightBottom(const Point& p){m_right = p.GetX();m_bottom = p.GetY();};
 	void SetCoord(int left,int top,int right,int bottom);
 	void SetRect(int x,int y,int w,int h);
 	
@@ -28,7 +29,8 @@ public:
 	inline bool IsInRect(const Rect& another)const;
 	inline bool IsIntersects(const Rect& another)const;			
 	inline Rect& Intersect(const Rect& another);			
-	inline Rect& Unite(const Rect& another);			
+	inline Rect& Unite(const Rect& another);
+	friend	std::ostream& operator<<(std::ostream& s,const Rect& r);		
 private:
 	int m_left;
 	int m_top;
@@ -43,6 +45,37 @@ inline bool Rect::IsEmpty()const
 inline bool Rect::IsValid()const
 {
 	return (m_left<=m_right) && (m_top <= m_bottom);
+}
+
+inline Point Rect::Center()const{
+	return Point((m_left+m_right)>>1,(m_top+m_bottom)>>1);
+}
+inline void Rect::SetCoord(int left,int top,int right,int bottom)
+{
+	m_left = left,m_top = top;
+	m_right = right,m_bottom = bottom;
+}
+inline void Rect::SetRect(int x,int y,int w,int h)
+{
+	m_left = x, m_top = y;
+	m_right = x+w;
+	m_bottom = y + h;
+}
+
+inline bool Rect::IsInRect(int x,int y)const
+{
+
+    return true;
+}
+
+inline bool Rect::IsInRect(const Rect& another)const{
+	return true;
+}
+
+std::ostream& operator<<(std::ostream& s,const Rect& r)
+{
+	s<<"Rect:("<<r.m_left<<","<<r.m_top<<"),("<<r.m_right<<","<<r.m_bottom<<")\n";
+	return s;
 }
 
 END_NAMESPACE
--- test_basic.cpp
+++ test_basic.cpp
@@ -1,5 +1,7 @@
-#include <core/point.h>
 #include <iostream>
+#include <core/point.h>
+#include <core/rect.h>
+
 
 using namespace std;
 
@@ -9,5 +11,8 @@ int main()
   typedef Point point_type;
   point_type point(100,100);
   cout<<point.GetX()<<","<<point.GetY()<<endl;
+  Rect r(100,100,150,200);
+  cout<<r;
+
 	return 0;
 }
